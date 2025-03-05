7e23e7fad29f60b7f10aab7fb3a382608a531f40
Roman <roma.bashirov.1@yandex.ru>
2025-03-05 16:51:10 +0300

    Update model_training.py

--- migration/model_training.py
+++ migration/model_training.py
@@ -48,5 +48,5 @@ def evaluate_model(model, test_data: dict) -> pd.DataFrame:
     
     return pd.DataFrame(
         res, 
-        index=[['Random Forest'] * 6, ['CPC', 'ACC', 'RMSE', 'RE', 'LR', 'DL(train)']]
+        index=[['Random Forest'] * 5, ['CPC', 'ACC', 'RMSE', 'RE', 'LR']]
     )
